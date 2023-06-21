b7a98fa8ca156831949dbbadf919aa44de3b0328
unknown <abvorobey@gmail.com>
2023-06-21 23:41:11 +0300

    домашнее 7

---
+++ HomeWork_7/ComplexNumberCalculator/src/App1.java
@@ -0,0 +1,11 @@
+
+
+public class App1 {
+    public static void main(String[] args) {
+       
+        iCalculate complexCalculator = new CalcDecorator(new Calculator(new ComplexNumber(0, 0)), new Logger());
+        View view = new View(complexCalculator);
+        view.run();
+
+    }
+}
---
+++ HomeWork_7/ComplexNumberCalculator/src/iCalculate.java
@@ -0,0 +1,10 @@
+
+
+public interface iCalculate {
+    
+    iCalculate sum(iComplexNumber arg);
+    iCalculate multi(iComplexNumber arg);
+    iCalculate subt(iComplexNumber arg);
+    iCalculate div(iComplexNumber arg);
+    iComplexNumber getResult();
+}
